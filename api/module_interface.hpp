a2d0c18ec12bba376db94debffa0d1c4eded5262
Maks UjGamer <ujgamer@yandex.com>
2017-03-08 02:26:59 +0300

    Module interface

--- api/module_interface.hpp
+++ api/module_interface.hpp
@@ -4,11 +4,34 @@
 #ifndef MBOT_MODULE_INTERFACE_HPP
 #define MBOT_MODULE_INTERFACE_HPP
 
+#include <memory>
+#include <string>
+#include <unordered_map>
 #include <tgbot/tgbot.h>
+#include "infastructure_facade.hpp"
 
 class module_interface {
+    using namespace TgBot;
 public:
-    virtual void 
+    struct command_info {
+        std::string synopsis;
+        std::string description;
+
+        bool admin_only = false;
+        bool creator_only = false;
+    };
+    using command_table = std::unordered_map<std::string, command_info>;
+
+    virtual std::string   get_nane() = 0;
+    virtual command_table get_command_table() { return command_table(); }
+
+    virtual void on_any_message(Message::Ptr message) {}
+    virtual void on_command(const std::string& command, const std::string parameters, Message::Ptr message) {}
+    virtual void on_non_command_message(Message::Ptr message) {}
+
+    virtual void on_init(std::unique_ptr<infastructure_facade> facade) {}
+    virtual void on_enabled() {}
+    virtual void on_disabled() {}
 };
 
 #endif //MBOT_MODULE_INTERFACE_HPP
